5c79901967e3b2162ace8dd5d4bbe14bac78f2f5
LUAN <110142201+LluanBDS@users.noreply.github.com>
2025-01-27 14:03:58 -0300

    criei uma funçao que verifica se existe a coluna quantidade no data_frame e mostra a quantia de produtos vendidos

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-# Substitua 'arquivo.xlsx' pelo caminho do seu arquivo Excel
-caminho_arquivo = 'arquivo.xlsx'
+# caminho para o arquivo do excel
+caminho_arquivo = "C:/Users/Luan/Desktop/dados_vendas_git.csv"
 
 # Leitura do Excel para DataFrame
-# Selecione a aba do Excel que deseja ler passando o nome da aba em `sheet_name`
-data_frame = pd.read_excel(caminho_arquivo)
+data_frame = pd.read_csv(caminho_arquivo, delimiter=';')
 
-# Exibe os dados lidos no prompt
-print(data_frame)
+#funçao que verifica se existe a coluna quantidade no data_frame e mostra a quantia de produtos vendidos
+def quantidade():
+    if "Quantidade" in data_frame.columns:
+        quantidae = data_frame["Quantidade"].sum().astype(int)
+        print(f"quantidade de produtos vendidos  = {quantidae}")
+    else:
+        print("vazio")
